9398d3423db63ed4a608a42c6595bb990f220b57
Cameron Little <cameron@camlittle.com>
2014-02-06 23:01:42 -0800

    now using wiringpi (no sudo required)

--- temp_humid_sensor.py
+++ temp_humid_sensor.py
@@ -1,26 +1,33 @@
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, call
 from threading import Thread
+import getpass
 import time, datetime
-import RPi.GPIO as GPIO
+import wiringpi
 import os
+GPIO = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_SYS)
 
 class Thermostat(object):
-    def __init__(self, pin, target_temp):
+    def __init__(self, out_pin, in_pin, target_temp=55):
         self.TARGET_TEMP = target_temp
         self.current_temp = None
         self.last_time = None
-        self.RELAY = pin
+        self.RELAY = out_pin
+        self.INPUT = in_pin
         self.RUNNING = True
         self.HEAT_ON = False
 
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.RELAY, GPIO.OUT)
-        GPIO.output(self.RELAY, False)
+        user = getpass.getuser()
+        call(["/usr/local/bin/gpio", "export", str(self.RELAY), "out"])
+        call(["/usr/local/bin/gpio", "export", str(self.INPUT), "in"])
+
+        GPIO.pinMode(self.RELAY, GPIO.OUTPUT)
+        GPIO.pinMode(self.RELAY, GPIO.INPUT)
+        GPIO.digitalWrite(self.RELAY, GPIO.LOW)
 
     def run(self):
         def run_():
             while self.RUNNING:
-                th_cmd = Popen(["./temp_humid_sensor", str(4)], stdout=PIPE)
+                th_cmd = Popen(["sudo", "./temp_humid_sensor", str(self.INPUT)], stdout=PIPE)
                 th_out, th_err = th_cmd.communicate()
 
                 if th_out != "err":
@@ -61,3 +68,24 @@ class Thermostat(object):
     def to_off(self):
         self.RUNNING = False
         GPIO.output(self.RELAY, False)
+
+if __name__ == '__main__':
+    import signal
+    thermostat = Thermostat(17, 4)
+    tt = thermostat.run()
+
+    def sig_handler(signal, frame):
+        print("Waiting for Thermostat to finish.")
+        tt.join()
+        sys.exit(0)
+    signal.signal(signal.SIGINT, sig_handler)
+
+    try:
+        tt = thermostat.run()
+        tt.start()
+        for i in range(0, 5):
+            print(thermostat.get_temp(), thermostat.get_last_time())
+            time.sleep(5)
+        tt.join()
+    finally:
+        pass
