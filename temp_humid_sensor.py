0f9149df73de63533bc2d15061ea164eae98fb43
Cameron Little <cameron@camlittle.com>
2014-02-10 08:21:15 -0800

    Now fully working with wiring pi and non root. Added file to allow running from external import

--- temp_humid_sensor.py
+++ temp_humid_sensor.py
@@ -3,71 +3,75 @@ from threading import Thread
 import getpass
 import time, datetime
 import os
-import wiringpi
-GPIO = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_SYS)
+import wiringpi2 as wiringpi
+import _cwd
+#wiringpi.wiringPiSetupGpio()
+wiringpi.wiringPiSetupSys()
 
 class Thermostat(object):
     def __init__(self, out_pin, in_pin, target_temp=55):
-        self.TARGET_TEMP = target_temp
+        self.target_temp = target_temp
         self.current_temp = None
         self.last_time = None
-        self.RELAY = out_pin
+        self.THERM = out_pin
         self.INPUT = in_pin
-        self.RUNNING = True
-        self.HEAT_ON = False
+        self.running = True
+        self.heat_on = False
+        self.OUT = 1
+        self.IN = 0
+        self.PWM = 0
 
-        user = getpass.getuser()
-        call(["/usr/local/bin/gpio", "export", str(self.RELAY), "out"])
-        call(["/usr/local/bin/gpio", "export", str(self.INPUT), "in"])
+        self.PATH = str(_cwd).split()[3][1:-10]
 
-        GPIO.pinMode(self.RELAY, GPIO.OUTPUT)
-        GPIO.pinMode(self.RELAY, GPIO.INPUT)
-        GPIO.digitalWrite(self.RELAY, GPIO.LOW)
+        call(["gpio", "export", str(self.THERM), "out"])
+        call(["gpio", "export", str(self.INPUT), "in"])
+
+        wiringpi.digitalWrite(self.THERM, 0)
 
     def run(self):
         def run_():
-            while self.RUNNING:
-                th_cmd = Popen(["sudo", "./temp_humid_sensor", str(self.INPUT)], stdout=PIPE)
+            while self.running:
+                th_cmd = Popen(["sudo", self.PATH + "/temp_humid_sensor", str(self.INPUT)], stdout=PIPE)
                 th_out, th_err = th_cmd.communicate()
 
-                if th_out != "err":
+                if th_out and th_out != "err":
                     self.last_time = time.strftime("%H:%M:%S")
                     temp, humid = th_out.split(',')
                     self.current_temp = (9.0 / 5.0) * float(temp) + 32
-                    if self.current_temp < self.TARGET_TEMP - 2 and not self.HEAT_ON:
-                        self.HEAT_ON = True
-                        GPIO.output(self.RELAY, self.HEAT_ON)
-                    elif self.current_temp >= self.TARGET_TEMP and self.HEAT_ON:
-                        self.HEAT_ON = False
-                        GPIO.output(self.RELAY, self.HEAT_ON)
+                    if self.current_temp < self.target_temp - 2 and not self.heat_on:
+                        self.heat_on = True
+                        wiringpi.digitalWrite(self.THERM, 1)
+                    elif self.current_temp >= self.target_temp and self.heat_on:
+                        self.heat_on = False
+                        wiringpi.digitalWrite(self.THERM, 0)
 
                 time.sleep(10)
         self.t = Thread(target=run_)
         return self.t
 
-    def set_(self, target_temp):
-        self.TARGET_TEMP = float(target_temp)
-        if self.current_temp and self.current_temp < self.TARGET_TEMP - 2 and not self.HEAT_ON:
-            self.HEAT_ON = True
-            GPIO.output(self.RELAY, self.HEAT_ON)
-        elif self.current_temp >= self.TARGET_TEMP and self.HEAT_ON:
-            self.HEAT_ON = False
-            GPIO.output(self.RELAY, self.HEAT_ON)
+    def set(self, target_temp):
+        self.target_temp = float(target_temp)
+        if self.current_temp and self.current_temp < self.target_temp - 2 and not self.heat_on:
+            self.heat_on = True
+            wiringpi.digitalWrite(self.THERM, 1)
+        elif self.current_temp >= self.target_temp and self.heat_on:
+            self.heat_on = False
+            wiringpi.digitalWrite(self.THERM, 0)
 
 
-    def get_temp(self):
+    def get(self):
         return self.current_temp
     def get_last_time(self):
         if self.last_time:
-            return " (" + self.last_time + ")"
+            return self.last_time
         else:
             return None
     def get_target(self):
-        return self.TARGET_TEMP
+        return self.target_temp
 
-    def to_off(self):
-        self.RUNNING = False
-        GPIO.output(self.RELAY, False)
+    def off(self):
+        self.running = False
+        wiringpi.digitalWrite(self.THERM, 0)
 
 if __name__ == '__main__':
     import signal
